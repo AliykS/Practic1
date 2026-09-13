0267304b8b22d58a2672eec85f42a35eebbd58b0
User <User@DESKTOP-6EL6Q07>
2026-09-13 23:05:13 +0300

    Добавьте файлы проекта.

---
+++ Practic1/MainWindow.axaml.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Practic1;
+using System.Collections.ObjectModel;
+
+namespace Practic1;
+
+public partial class MainWindow : Window
+{
+    private ObservableCollection<Museum> museums = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        MuseumsListBox.ItemsSource = museums;
+    }
+
+    private void AddButton_Click(object? sender, RoutedEventArgs e)
+    {
+        string museumName = MuseumNameTextBox.Text?.Trim() ?? "";
+        string city = CityTextBox.Text?.Trim() ?? "";
+
+        if (string.IsNullOrWhiteSpace(museumName) ||
+            string.IsNullOrWhiteSpace(city))
+        {
+            ErrorTextBlock.Text = "Ошибка: заполните название музея и город.";
+            return;
+        }
+
+        ErrorTextBlock.Text = "";
+
+        Museum museum = new Museum
+        {
+            Name = museumName,
+            City = city
+        };
+
+        museums.Add(museum);
+
+        MuseumNameTextBox.Text = "";
+        CityTextBox.Text = "";
+    }
+}
---
+++ Practic1/Museum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practic1
+{
+    public class Museum
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
